9af039524f77da0c7bcb915a76c9f62922d56fa2
Moamen <mmoamenmohamed101@gmail.com>
2024-10-05 00:21:12 +0300

    name

---
+++ lib/name.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Name extends StatelessWidget {
+  const Name({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
