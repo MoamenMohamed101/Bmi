9fe4e2cef05db07fd4033dc7bf1829ec5c55758d
Moamen <mmoamenmohamed101@gmail.com>
2024-10-05 17:04:13 +0300

    white screen

---
+++ lib/white_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WhiteScreen extends StatelessWidget {
+  const WhiteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
